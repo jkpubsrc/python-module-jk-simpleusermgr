99a622e0c0386aba689d8d80241ae0c754e35c3c
jkpubsrc <pubsrc@binary-overflow.de>
2020-04-16 12:58:05 +0200

    Initial release.

---
+++ testing/test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+from jk_simpleusermgr import *
+
+
+
+userMgr = UserMgr("data")
+u1 = userMgr.getCreateUser("testuser123")
+u2 = userMgr.getCreateUser("testuserABC")
+
+
+
+
+
+
+
+
